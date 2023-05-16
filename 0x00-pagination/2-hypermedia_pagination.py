3cb043c52570ee778782d2e1dab6764196efc527
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-16 01:10:09 +0100

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -61,7 +61,11 @@ class Server:
             n = None
         else:
             n = page + 1
+        if page - 1 == 0:
+            p = None
+        else:
+            p = page - 1
         d = {"page_size": page_size, "page": page, "data": self.get_page(page, page_size), 
-            "next_page": n, "prev_page": page - 1, "total_pages": total_pages}
+            "next_page": n, "prev_page": p, "total_pages": total_pages}
         return d
 
