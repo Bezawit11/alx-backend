d431f00af6d0ff35631bf1e1efffc34ec7fe972e
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-17 18:29:55 +0100

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-"""1-fifo_cache.py"""
+"""3-lru_cache.py"""
 
+
+from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
     """inherits from BaseCaching and is a caching system
     """
-    m = []
+    cache = OrderedDict()
 
     def __init__(self):
         """Initializes"""
         super().__init__()
 
     def put(self, key, item):
-        """ assign to the dict cache_data the item value for the key"""
+        """assign to the dict cache_data the item value for the key"""
         if key is None or item is None:
             return
-        
+        self.cache[key] = item
+        self.cache.move_to_end(key)
+        if len(self.cache) > BaseCaching.MAX_ITEMS:
+            a = list(self.cache.keys())[0]
+            #print(self.cache)
+            print(f"DISCARD: {a}")
+            self.cache.popitem(last = False)
+        self.cache_data = self.cache.copy()
 
     def get(self, key):
         """gets values from dictionary by referencing a key
         """
         if key is None:
             return None
-        if key in self.cache_data.keys():
-            v = self.cache_data[key]
-            return v
+        if key not in self.cache:
+            return
+        self.cache.move_to_end(key)
+        return self.cache[key]
