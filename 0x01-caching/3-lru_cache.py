0341d5d9f26600fb461d97ad35d6b5940b6b3cdc
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-17 11:59:27 +0100

    Update 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""1-fifo_cache.py"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system
+    """
+    m = []
+
+    def __init__(self):
+        """Initializes"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ assign to the dict cache_data the item value for the key"""
+        if key is None or item is None:
+            return
+        
+
+    def get(self, key):
+        """gets values from dictionary by referencing a key
+        """
+        if key is None:
+            return None
+        if key in self.cache_data.keys():
+            v = self.cache_data[key]
+            return v
