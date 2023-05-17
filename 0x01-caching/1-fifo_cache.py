9b21c0341f9046e9a683580428cc1da4db0357c2
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-17 08:36:07 +0100

    Update 1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""1-fifo_cache.py"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """Initializes"""
+        super().__init__()
+    
+    def put(self, key, item):
+        """ assign to the dict cache_data the item value for the key"""
+        if key is None or item is None:
+            return
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            a = list(self.cache_data.keys())[0]
+            self.cache_data.pop(a)
+            print(f"DISCARD: {a}")
+
+    def get(self, key):
+        """gets values from dictionary by referencing a key
+        """
+        if key is None:
+            return None
+        if key in self.cache_data.keys():
+            v = self.cache_data[key]
+            return v
