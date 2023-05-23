e29d0a5fa6a05103fb1abd7028b4b5a2821944cf
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-23 21:53:29 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,7 +6,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('0-index.html')
 
+@app.route('/')
+def index() -> str:
+    """homepage"""
+    return render_template('0-index.html')
