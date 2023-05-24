248bf92b640bea9844b409929567b4be6bc58a62
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-24 08:16:39 +0100

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -27,7 +27,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """The home page"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
