3cc829c161c0764f1a804e793ae6fde87d8f11cb
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-05-24 08:13:55 +0100

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,3 +22,7 @@ babel = Babel(app)
 def get_locale() -> str:
     """Get locale from request"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
