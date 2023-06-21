7753f73d1f1300c38b4221246d15e0da0dea48bb
Bezawit11 <socbeza13@gmail.com>
2023-06-21 08:37:11 +0000

    task 2

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,20 @@
+const redis = require('redis');
+const redisClient = redis.createClient();
+redisClient.on('ready', () => {
+    console.log("Redis client connected to the server");
+});
+redisClient.on('error', (err) => {
+    console.error("Redis client not connected to the server: ${err}");
+});
+const setNewSchool = (schoolName, value) => {
+  redisClient.SET(schoolName, value);
+};
+const displaySchoolValue = (schoolName) => {
+  console.log(redisClient.GET(schoolName));
+};
+
+
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
