30e677323a0a3bf1c7fe86f56d98d3e36d337b06
Bezawit11 <socbeza13@gmail.com>
2023-06-21 07:33:32 +0000

    task 1

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+const r = require('redis')
+const redisClient = r.createClient()
+(async () => {
+    await rediClient.connect();
+})();
+redisClient.on('ready', () => {
+    console.log("Redis client connected to the server");
+});
+redisClient.on('error', (err) => {
+    console.error("Redis client not connected to the server: ${err}");
+});
