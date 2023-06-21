539a2c710618449da16fdf513d84ece96f32d0ec
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-21 15:14:54 +0100

    Update 5-publisher.js

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -4,17 +4,13 @@ publisher.on('ready', () => {
     console.log("Redis client connected to the server");
 });
 publisher.on('error', (err) => {
-    console.error("Redis client not connected to the server: ${err}");
+    console.error("Redis client not connected to the server:", err);
 });
-(async () => {
-
-  const article = {
-    id: '123456',
-    name: 'Using Redis Pub/Sub with Node.js',
-    blog: 'Logrocket Blog',
-  };
-
-  await publisher.connect();
-
-  await publisher.publish('article', JSON.stringify(article));
-})();
+const greet = () => {
+  console.log("About to send MESSAGE");
+};
+const publishMessage = (message, time) => {
+  setTimeout(greet, time);
+  publisher.connect();
+  publisher.publish('holberton school channel', JSON.stringify(message));
+};
