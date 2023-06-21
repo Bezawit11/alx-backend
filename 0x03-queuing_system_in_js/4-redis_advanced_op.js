150caeacb0c4cd39759ab2da5fff2eaf3e11a78d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-06-21 14:50:18 +0100

    Update 4-redis_advanced_op.js

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -7,12 +7,12 @@ redisClient.on('error', (err) => {
     console.error("Redis client not connected to the server: ${err}");
 });
 
-redisClient.hmset("HolbertonSchools",
+redisClient.HMSET("HolbertonSchools",
 		   "Portland", 50,
 		   "Seattle", 80,
 		   "New York", 20,
 		   "Bogota", 20,
 	 	   "Cali", 40,
 		   "Paris", 2)
-const result = redisClient.hmget("HolbertonSchools", "Portland");
+const result = redisClient.HGET("HolbertonSchools", "Portland");
 console.log(result);
